7f2d205e9cd5a6cac2cfb049fb183bcdc2577ff1
PMH Studio / CS <hj666c3@gmail.com>
2019-05-05 00:34:50 +0900

    백업

--- koreanscript csharp/koreanscript csharp/변수제어.cs
+++ koreanscript csharp/koreanscript csharp/변수제어.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace koreanscript_csharp
 {
     class 변수제어
     {
-        public void 값지정()
+        public void 값지정(string[] 띄어쓰기)
         {
+            string 변수 = File.ReadAllText("변수.json");
+            JObject 변수읽기 = JObject.Parse(변수);
+            try
+            {
+                int 정수출력;
+                decimal 실수출력;
+                string 문자출력;
+                string 타입 = 변수읽기[띄어쓰기[1]]["type"].ToString();
+                string 값 = 변수읽기[띄어쓰기[1]]["value"].ToString();
+                switch(타입)
+                {
+                    case "int":
+                        if (int.TryParse(값, out 정수출력))
+                        {
 
+                        }
+                        break;
+                }
+            }
+            catch { }
         }
     }
 }
